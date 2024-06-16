69677c8e017bcd678c7e61d695e66cb180a7edb5
Marten Schulz <ms@marten-schulz.de>
2024-06-16 17:24:23 +0200

    Refactor admin.py to use custom UserCreationForm and UserChangeForm

--- admin.py
+++ admin.py
@@ -1,25 +1,40 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User, Restaurant
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+class UserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'role', 'restaurants', 'birthdate', 'is_active')
+
+class UserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'role', 'restaurants', 'birthdate', 'is_active')
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
     model = User
-    list_display = ['username', 'email', 'role', 'is_staff', 'is_active']
-    list_filter = ['role', 'is_staff', 'is_active']
+
+    list_display = ('username', 'email', 'role', 'birthdate', 'is_active')
+    list_filter = ('role', 'restaurants', 'is_active')
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password', 'role')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'birthdate')}),
+        ('Permissions', {'fields': ('role', 'groups', 'user_permissions', 'is_active')}),
+        ('Restaurants', {'fields': ('restaurants',)}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'role', 'is_staff', 'is_active')}
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'restaurants', 'birthdate', 'is_active')}
         ),
     )
-    search_fields = ('email', 'username')
-    ordering = ('email',)
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions', 'restaurants')
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, UserAdmin)
+
--- urls.py
+++ urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
+    #path('register/', views.register_view, name='register'),
+    #path('login/', views.login_view, name='login'),
     path('users/', views.user_list, name='user_list'),
     path('users/create/', views.create_user, name='create_user'),
     path('users/update/<int:pk>/', views.update_user, name='update_user'),
