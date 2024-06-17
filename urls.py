0017365d74a643cd17301e4277ecd99b0aa016bc
Marten Schulz <ms@marten-schulz.de>
2024-06-17 21:12:19 +0200

    chore: Remove commented out code in urls.py

--- urls.py
+++ urls.py
@@ -2,10 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('register/', views.register_view, name='register'),
-    #path('login/', views.login_view, name='login'),
-    #path('users/', views.user_list, name='user_list'),
-    #path('users/create/', views.create_user, name='create_user'),
-    #path('users/update/<int:pk>/', views.update_user, name='update_user'),
-    #path('users/delete/<int:pk>/', views.delete_user, name='delete_user'),
+
 ]
