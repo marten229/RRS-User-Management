d4edd30f85427637dfbed4838c8f31071f857ffc
Marten Schulz <ms@marten-schulz.de>
2024-06-17 18:02:02 +0200

    feat: Add first name, last name, and birthdate fields to registration form

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'birthdate', 'password1', 'password2']
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
